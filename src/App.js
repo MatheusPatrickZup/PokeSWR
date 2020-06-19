acea8f6dfa098de809ab303f7feee58883459f4b
Matheus Patrick <matheus.patrick@zup.com.br>
2020-06-19 12:18:55 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import useSWR from "swr";
+
+const randomPokemon = Math.floor(1 + Math.random() * 600);
 
 function App() {
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data: pokemon } = useSWR(
+    "https://pokeapi.co/api/v2/pokemon/" + randomPokemon,
+    fetcher
+  );
+  const { data: species } = useSWR(() => pokemon.species.url, fetcher);
+
+  const getPokemonName = () =>
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const getFlavorText = () =>
+    species.flavor_text_entries.find((entry) => entry.language.name === "en")
+      .flavor_text;
+
+  if (!species) {
+    return null;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={pokemon && pokemon.sprites.front_default}
+          className="App-logo"
+          alt="logo"
+        />
+        <span className="pokemon-name">{getPokemonName()}</span>
+        <span className="pokemon-desc">{unescape(getFlavorText())}</span>
       </header>
     </div>
   );
